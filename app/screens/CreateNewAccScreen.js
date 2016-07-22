2cd4605fef85da77b76a2e29e30f763bdc1e5c6b
Kaiwen <huangkaiw3n@gmail.com>
2016-07-22 18:10:27 +0800

    hooked up textinputs on create acc screen

--- app/screens/CreateNewAccScreen.js
+++ app/screens/CreateNewAccScreen.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react'
 import { View, Text, StyleSheet, Platform, TouchableOpacity } from 'react-native'
 import ViewContainer from '../components/ViewContainer'
 import HeaderBarWithLeftTouchableIcon from '../components/HeaderBarWithLeftTouchableIcon'
+import MaterialsIcon from 'react-native-vector-icons/MaterialIcons';
+import { Fumi } from 'react-native-textinput-effects';
 
 class CreateNewAccScreen extends Component{
   constructor(props){
@@ -17,9 +19,39 @@ class CreateNewAccScreen extends Component{
           CREATE A NEW ACCOUNT
         </HeaderBarWithLeftTouchableIcon>
 
-        <View style={{flex:9}}>
-
-
+        <View style={{flex:9, justifyContent:"flex-start", }}>
+          <Fumi
+            label={'Name'}
+            iconClass={MaterialsIcon}
+            iconName={'account-circle'}
+            iconColor={'#f95a25'}
+            keyboardType={'email-address'}
+          />
+          <Fumi
+            style={{ marginTop: 3 }}
+            label={'Email'}
+            iconClass={MaterialsIcon}
+            iconName={'email'}
+            iconColor={'#f95a25'}
+            keyboardType={'email-address'}
+          />
+          <Fumi
+            style={{ marginTop: 3}}
+            label={'Password'}
+            iconClass={MaterialsIcon}
+            iconName={'lock'}
+            iconColor={'#f95a25'}
+            secureTextEntry={true}
+            keyboardType={'default'}
+          />
+          <Fumi
+            style={{ marginTop: 3 }}
+            label={'Mobile No.'}
+            iconClass={MaterialsIcon}
+            iconName={'smartphone'}
+            iconColor={'#f95a25'}
+            keyboardType={'numeric'}
+          />
         </View>
 
         <TouchableOpacity onPress={this.onClick} style={[styles.headerFooterBar, {justifyContent:"center"}]}>
