5f850554b969ba8f8133260466cd175ad613f307
Kaiwen <huangkaiw3n@gmail.com>
2016-07-25 16:36:19 +0800

    made android entry point identical code as ios

--- index.android.js
+++ index.android.js
@@ -5,15 +5,45 @@
  */
 
 import React, { Component } from 'react'
-import { AppRegistry, StyleSheet, Text } from 'react-native';
+import { View, AppRegistry, StyleSheet, Text, Navigator } from 'react-native';
 import WelcomeScreen from './app/screens/WelcomeScreen'
+import CreateNewAccScreen from './app/screens/CreateNewAccScreen'
+import HeaderBarWithLeftTouchableIcon from './app/components/HeaderBarWithLeftTouchableIcon'
+import MapNaviScreen from './app/screens/MapNaviScreen'
 
 class StreetSmart extends Component {
+
+  _renderScene(route, navigator){
+    var globalNavigatorProps = { navigator }
+
+    switch(route.identifier){
+      case "WelcomeScreen":
+        return <WelcomeScreen {...globalNavigatorProps}/>
+      case "CreateNewAccScreen":
+        return <CreateNewAccScreen {...globalNavigatorProps}/>
+      case "MapNaviScreen":
+        return <MapNaviScreen {...globalNavigatorProps}/>
+      default:
+        return <View style={{flex:1, color:"red"}}/>
+      }
+
+    }
+
   render() {
     return (
-      <WelcomeScreen></WelcomeScreen>
+      <Navigator
+        initialRoute = {{identifier: "MapNaviScreen"}}
+        ref="appNavigator"
+        style={styles.navigatorStyles}
+        renderScene={this._renderScene} />
     );
   }
 }
+
+const styles = StyleSheet.create({
+  navigatorStyles:{
+
+  }
+})
 
 AppRegistry.registerComponent('StreetSmart', () => StreetSmart);
