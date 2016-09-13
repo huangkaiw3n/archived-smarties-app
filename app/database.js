ce772d7367d4aaee1645c486d7f953ac42aeb6c8
Kaiwen <huangkaiw3n@gmail.com>
2016-09-13 18:00:23 +0800

    Both configs available
    # Conflicts:
    #	app/database.js

---
+++ app/database.js
@@ -0,0 +1,29 @@
+import * as Firebase from 'firebase'
+
+let HAS_INITIALIZED = false
+
+const initFirebase = () => {
+    if (!HAS_INITIALIZED) {
+        const config = {
+          apiKey: "AIzaSyD8teYh60MnxpQso5uFyWLsZcmtvbXYw0U",
+           authDomain: "smarties-ebaab.firebaseapp.com",
+           databaseURL: "https://smarties-ebaab.firebaseio.com",
+           storageBucket: "",
+        };
+        // const config = {
+        //   apiKey: "AIzaSyBzClvv2oiYFWRv1AbpwXz_KK84WiOLA-s",
+        //   authDomain: "parkpark-334b8.firebaseapp.com",
+        //   databaseURL: "https://parkpark-334b8.firebaseio.com",
+        //   storageBucket: "",
+        // };
+
+        Firebase.database.enableLogging(true)
+        Firebase.initializeApp(config)
+        HAS_INITIALIZED = true
+    }
+}
+
+export const getDatabase = () => {
+    initFirebase()
+    return Firebase.database()
+}
